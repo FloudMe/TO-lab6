4f69907dcf26df98a05b7ef239825b78537e5926
Damian Marecik <marecik1999@gmail.com>
2020-12-15 12:26:23 +0100

    Small changes

--- src/com/FireBrigade/Main.java
+++ src/com/FireBrigade/Main.java
@@ -14,11 +14,11 @@ public class Main {
 
         VFDUnit osp2 = new VFDUnit("kr2","TEST", "ALARM");
         osp2.add(new Firefighter("Krzysztof", "Jarzyna", "333"));
-        osp2.add(new Firefighter("Marcin", "Bolek", "9997"));
+        osp2.add(new Firefighter("Marcin", "Bolek", "444"));
 
         VFDUnit osp3 = new VFDUnit("kr3","TEST", "ALARM");
-        osp3.add(new Firefighter("Mateusz", "Borek", "997"));
-        osp3.add(new Firefighter("Tomasz", "Smokowski", "9997"));
+        osp3.add(new Firefighter("Mateusz", "Borek", "555"));
+        osp3.add(new Firefighter("Tomasz", "Smokowski", "666"));
 
         Message.notifyVFDUnit(osp1, "ALARM");
         Message.notifyVFDUnit(osp2, "TEST");
